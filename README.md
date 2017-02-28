1b924b7d18e34aa4024e2e1c2db498704b73b2c6
Pascal Garber <pascal@jumplink.eu>
2017-02-28 14:49:46 +0000

    change auth for instagram, still contains old stuff and the templates are very rudimentary, but works

--- README.md
+++ README.md
@@ -1,64 +1,63 @@
-# Use Instagram Sign In with Firebase
+# Microservice to use Instagram in JumpLink Themes
 
-This sample shows how to Sign in Firebase using [Instagram](https://www.instagram.com/developer/).
+We have developed this service to easily integrate a instagram account to our customers themes.
 
-You can try this sample at: [instagram-auth.appspot.com](https://instagram-auth.appspot.com).
+# Host yourself
+If you wanna host this microservice for your own themes follow this instructions:
 
+## Create a App
+By default this microservice is designed for two types of theme integration apps:
+ * For Shopify Themes
+ * For OctoberCMS Plugin
+ * For Client-Side integration using Instafeed
 
-## Setup the sample
-
-### Instagram app's creation and setup
-
- 1. Register an Instagram app on [Instagram for Developers](https://www.instagram.com/developer/). You'll need to **Register a New Client**.
- 1. Once Your app is created make sure you specify your app's callback URL in the list of **Valid redirect URIs** of your Instagram app. You should whitelist `http://localhost:8080/instagram-callback` for local development and if you deploy on App Engine (See Deploy section below) you should whitelist the URL `https://<project-id>.appspot.com/instagram-callback`.
- 1. Copy the **Client ID** and the **Client Secret** of your Instagram app and copy it into `config.json` in place of the placeholders.
-
-
-### Firebase app creation and setup
-
- 1. Create a Firebase project using the [Firebase Developer Console](https://console.firebase.google.com).
- 1. **For the web app:** Copy the Web initialisation snippet from **Firebase Console > Overview > Add Firebase to your web app** and paste it in `public/index.html` in lieu of the placeholders (where the `TODO(DEVELOPER)` is located).
- 1. **For the server:** From the Firebase initialization snippet copy the `apiKey` value and paste it in `config.json` as the value of `firebase.apiKey` in lieu of the placeholder.
-
-Create and provide a Service Account's keys:
- 1. Create a Service Accounts file as described in the [Server SDK setup instructions](https://firebase.google.com/docs/server/setup#add_firebase_to_your_app).
- 1. Save the Service Account credential file as `service-account.json`
-
-Deploy your security rules:
- 1. Run `firebase use --add` and choose your Firebase project. This will configure the Firebase CLI to use the correct
-    project locally.
- 1. Run `firebase deploy` to deploy the security rules of the Realtime Database
+If you want to add your own you need to integrate your in three places of this microservice:
 
 
-## Run the sample locally
+### config.json
+By default or config.json looks like:
 
-### Web
+```json
+{
+    "instagramApps": {
+        "shopify": {
+            "name": "JumpLink Shopify Theme Integration",
+            "id": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+            "secret": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+            "tokenHost": "https://api.instagram.com",
+            "tokenPath": "/oauth/access_token",
+            "callbackPath": "/instagram-callback",
+            "scopes": "basic public_content"
+        },
+        "october": {
+            "name": "JumpLink OctoberCMS Theme Integration",
+            "id": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+            "secret": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+            "tokenHost": "https://api.instagram.com",
+            "tokenPath": "/oauth/access_token",
+            "callbackPath": "/instagram-callback",
+            "scopes": "basic public_content"
+        },
+        "instafeed": {
+            "name": "JumpLink Instafeed Integration",
+            "id": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+            "secret": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+            "tokenHost": "https://api.instagram.com",
+            "tokenPath": "/oauth/access_token",
+            "callbackPath": "/instagram-callback",
+            "scopes": "basic public_content"
+        }
 
-You can run the sample web app locally by running `npm run start` in a console.
+    }
+}
+```
 
-Then open `http://localhost:8080` in your browser.
+You need to add your integration app name like we have with 'shopify', 'october' and 'instafeed'.
 
-Click on the **Sign in with Instagram** button and a popup window will appear that will let you sign in with your Instagram account.
+### views/setup/[appName].pup
 
-After signing-in the app should display your name, profile pic and Instagram Pics. At this point you are authenticated in Firebase and can use the database/hosting etc...
-The app shows a simple
+Here are the templates which are displayed before making a token request, each integration app from the config.json need to must a template file included here.
+We are using the [pug](https://pugjs.org) template engine here.
 
-
-## Deploy the sample
-
-You can deploy the sample's web app and server on App Engine Flex. For this:
- - Download and setup the [gcloud](https://cloud.google.com/sdk/) CLI.
- - Make sure Billing is enabled in your Google project and that Google Compute Instance is also enabled.
- - Run `cd server & gcloud app deploy`
-
- Your app will be available under your App Engine URL: `https://<project-id>.appspot.com`
-
-
-## Contributing
-
-We'd love that you contribute to the project. Before doing so please read our [Contributor guide](../CONTRIBUTING.md).
-
-
-## License
-
-© Google, 2016. Licensed under an [Apache-2](../LICENSE) license.
+### views/result/[appName].pup
+The same rules apply as the setup template files. But these templates display the result token.
---
+++ microservice-instagram.js
@@ -0,0 +1,276 @@
+/**
+ * @see https://firebase.googleblog.com/2016/10/authenticate-your-firebase-users-with.html
+ * @see https://github.com/firebase/custom-auth-samples
+ * @see http://gavinballard.com/shopify-oauth-flow-for-dummies/
+ * @see https://console.firebase.google.com/project/tagged-images/overview
+ * @todo https://firebase.google.com/docs/cli/
+ * @todo https://github.com/Daplie/node-letsencrypt
+ * @see https://github.com/OptimalBits/redbird
+ */
+
+'use strict';
+
+const PORT = process.env.PORT || 3050;
+var session = require('express-session');
+const Firebase = require('firebase-admin');
+const express = require('express');
+const crypto = require('crypto');
+var config = require('./config');
+const oauth2 = require('simple-oauth2');
+
+var clientApp = {};
+for(var instagramApp in config.instagramApps) {
+  var credentials = {
+    client: {
+      id: config.instagramApps[instagramApp].id,
+      secret: config.instagramApps[instagramApp].secret
+    },
+    auth: {
+      tokenHost: config.instagramApps[instagramApp].tokenHost,
+      tokenPath: config.instagramApps[instagramApp].tokenPath
+    }
+  };
+  // console.log("shopify instagramApp", instagramApp, credentials);
+  clientApp[instagramApp] = oauth2.create(credentials);
+  clientApp[instagramApp].callbackPath = config.instagramApps[instagramApp].callbackPath;
+  clientApp[instagramApp].scopes = config.instagramApps[instagramApp].scopes;
+}
+
+var firebase = {};
+if(config.firebase) {
+  for(var appName in config.firebase){
+    // console.log("firebase appName", appName);
+
+    if(config.firebase[appName] && config.firebase[appName]['service-account']) {
+      firebase[appName] = Firebase.initializeApp({
+        serviceAccount: config.firebase[appName]['service-account'],
+      }, appName);
+    } else {
+      console.warn("No service-account for firebase found!", appName);
+    }
+  }
+} else {
+  console.warn("No Config for firebase found!");
+}
+
+var app = express();
+
+// http://expressjs.com/de/guide/using-template-engines.html
+app.set('view engine', 'pug');
+
+app.use(session({
+  secret: 'RA~9$RLsN3&5svsynjpj+x',
+  saveUninitialized: false,
+  resave: false
+}));
+
+/**
+ * To test if this microservice is running
+ */
+app.get('/hello', function (req, res) {
+  res.send('world');
+});
+
+app.get('/setup/:appName', function (req, res) {
+  res.render(`setup/${req.params.appName}`, {appName: req.params.appName});
+});
+
+/**
+ * Redirects the User to the Instagram authentication consent screen. Also the 'state' session is set for later state
+ * verification.
+ */
+app.get('/redirect/:appName/', function (req, res) {
+  //if (req.session.token) return res.send('Token ready to be used: '+req.session.token);
+
+  var appName = req.params.appName; // e.g. shopify or october
+
+  if(!req.session[appName]) {
+    req.session[appName] = {};
+  }
+
+  //
+  // Generate a random nonce.
+  //
+  var state = crypto.randomBytes(20).toString('hex');
+
+  //
+  // Generate the authorization URL. For the sake of simplicity the shop name
+  // is fixed here but it can, of course, be passed along with the request and
+  // be different for each request.
+  //
+  // console.log('generate auth url for:', appName);
+  var uri = clientApp[appName].authorizationCode.authorizeURL({
+    redirect_uri: `https://${req.get('host')}${clientApp[appName].callbackPath}/${appName}`,
+    scope: clientApp[appName].scopes,
+    state: state
+  });
+
+  //
+  // Save the nonce in the session to verify it later.
+  //
+  req.session[appName].state = state;
+  // console.log('Redirecting to:', uri);
+  res.redirect(uri);
+});
+
+/**
+ * Exchanges a given Shopify auth code passed in the 'code' URL query parameter for a Firebase auth token.
+ * The request also needs to specify a 'state' query parameter which will be checked against the 'state' cookie to avoid
+ * Session Fixation attacks.
+ * This is meant to be used by Web Clients.
+ */
+app.get('/instagram-callback/:appName', function (req, res) {
+  var state = req.query.state;
+  var code = req.query.code;
+  var appName = req.params.appName;
+  var oldState = req.session[appName].state;
+ 
+  if(!req.session[appName]) {
+    req.session[appName] = {};
+  }
+
+  // Validate the state.
+  if ( typeof state !== 'string' || state !== oldState) {
+    console.error('Security checks failed on state', state, oldState);
+    return res.status(400).send('Security checks failed on state');
+  }
+
+  var redirect_uri = `https://${req.get('host')}${clientApp[appName].callbackPath}/${appName}`;
+  // console.log('redirect_uri', redirect_uri);
+
+  //
+  // Exchange the authorization code
+  // 
+  clientApp[appName].authorizationCode.getToken({
+    code: req.query.code,
+    redirect_uri: redirect_uri,
+  }).then(results => {
+    // console.log('Auth code exchange result received:', results);
+    // We have an Instagram access token and the user identity now.
+    const instagramAccessToken = results.access_token;
+    const instagramUserID = results.user.id;
+
+    var user = {
+      profilePic: results.user.profile_picture,
+      fullName: results.user.full_name,
+      username: results.user.username,
+      website: results.user.website,
+      bio: results.user.bio
+    }
+
+    // Create a Firebase account and get the Custom Auth Token.
+    createFirebaseAccount(appName, instagramUserID, user, instagramAccessToken, function (err, firebaseAuth, instagramAccessToken) {
+
+      if(err) {
+        return res.jsonp({
+          status: 500,
+          message: err
+        });
+      }
+
+      // If no firebaseAuth is set just return the accessToken without saving the token in firebase
+      if(!firebaseAuth || firebaseAuth === null) {
+        return res.render(`result/${req.params.appName}`, {appName: req.params.appName, instagramAccessToken: instagramAccessToken});
+      }
+
+      req.session[appName].firebaseToken = firebaseAuth.token;
+      req.session[appName].firebaseUid = firebaseAuth.uid;
+      req.session[appName].instagramToken = token;
+      req.session[appName].state = undefined;
+
+      // Serve an HTML page that signs the user in and updates the user profile.
+      res.send(signInFirebaseTemplate(req.session[appName].firebaseToken, appName));
+    });
+    
+  });
+
+});
+
+app.get('/token/:appName', function (req, res) {
+
+  var appName = req.params.appName;
+
+  if( req.session[appName] && req.session[appName].firebaseToken ) {
+    return res.jsonp({
+      firebaseToken: req.session[appName].firebaseToken,
+      // firebaseUid: req.session[appName].firebaseUid,
+      // instagramToken: req.session[appName].instagramToken
+    });
+  }
+
+    return res.jsonp({
+      status: 404,
+      message: 'Not Found'
+    });
+
+
+});
+
+/**
+ * Not used fpr shopify, october and instafeed!
+ * 
+ * Creates a Firebase account with the given user profile and returns a custom auth token allowing
+ * signing-in this account.
+ * Also saves the accessToken to the datastore at /instagramAccessToken/$uid
+ *
+ * @returns {Promise<string>} The Firebase custom auth token in a promise.
+ */
+var createFirebaseAccount = function (appName, instagramID, user, instagramAccessToken, cb) {
+  // The UID we'll assign to the user.
+  var uid = `instagram:${instagramID}`;
+
+  // Save the access token tot he Firebase Realtime Database.
+  if(firebase[appName])  {
+    const databaseTask = firebase[appName].database().ref(`/instagramAccessToken/${uid}`).set(instagramAccessToken);
+
+    // Create or update the user account.
+    const userCreationTask = firebase[appName].auth().updateUser(uid, {
+      displayName: displayName,
+      photoURL: photoURL
+    }).catch(error => {
+      // If user does not exists we create it.
+      if (error.code === 'auth/user-not-found') {
+        user.uid = uid;
+        return firebase[appName].auth().createUser(user);
+      }
+      return cb(error, null);
+    }).then(firebaseToken => {
+       return cb(null, firebaseToken, instagramAccessToken);
+    });
+  } else {
+    console.warn(`no firebase app for ${appName}`);
+    return cb(null, null, instagramAccessToken);
+  }
+}
+
+var getclientAppUrl = function (shop, apiKey) {
+  return 'https://'+shop+'/admin/apps/'+apiKey
+}
+
+/**
+ * Not used fpr shopify, october and instafeed!
+ * 
+ * Generates the HTML template that:
+ *  - Signs the user in Firebase using the given token
+ *  - Updates the user profile with shop
+ *  - Saves the Shopify AccessToken to the Realtime Database
+ *  - Closes the popup
+ */
+var signInFirebaseTemplate = function (firebaseToken, appName) {
+  return `
+    <script src="https://www.gstatic.com/firebasejs/3.6.2/firebase.js"></script>
+    <script>
+      var firebaseToken = '${firebaseToken}';
+      var config = {
+        apiKey: '${config.firebase[appName].apiKey}'
+      };
+      var app = firebase.initializeApp(config);
+      app.auth().signInWithCustomToken(firebaseToken).then(function() {
+        window.close();
+      });
+    </script>`;
+}
+
+app.listen(PORT, function () {
+  console.log('Open http://localhost:'+PORT+'/hello or https://dev.instagram.api.jumplink.eu/hello in your browser');
+});
